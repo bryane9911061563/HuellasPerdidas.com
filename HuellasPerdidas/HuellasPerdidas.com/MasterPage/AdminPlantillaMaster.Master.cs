02542e3dc16770046d92933c25bb41bfaef46096
Bryan Edilberto <bryanedilberto@hotmail.com>
2019-07-01 09:37:55 -0500

    SuboRepo

---
+++ HuellasPerdidas/HuellasPerdidas.com/MasterPage/AdminPlantillaMaster.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HuellasPerdidas.com.MasterPage
+{
+    public partial class AdminPlantillaMaster : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
